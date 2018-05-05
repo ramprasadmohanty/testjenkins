4c05ec3d8f82dd682e9ebb77ccc48bea9d813ed3
ramprasadmohanty <ram.mohanty@hotmail.com>
2018-05-05 16:25:41 +0530

    first time project for jenkins

---
+++ src/main/java/com/prject/App.java
@@ -0,0 +1,12 @@
+package com.prject;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World");
+		// This is for practice.
+
+	}
+
+}
---
+++ src/main/java/com/prject/FrontScreen.java
@@ -0,0 +1,12 @@
+package com.prject;
+
+public class FrontScreen {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is my fron screen");
+		// TODO Auto-generated method stub
+
+	}
+
+}
